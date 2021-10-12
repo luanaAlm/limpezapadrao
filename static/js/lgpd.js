b64f070418dcfba08705c712b8b4ee150b242948
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-10-12 13:02:43 -0300

    LGPD

---
+++ static/js/lgpd.js
@@ -0,0 +1,36 @@
+let lgpdUrl = 'https://limpezapadrao.herokuapp.com/';
+
+let lgpdHtml = `
+    <div class="lgpd" class="fixed">
+        <div class="lgpd--left">
+            Utilizamos cookies essenciais para melhor expriência do usuário,  
+            para conferir detalhadamente leia nossa <b> <a href="">Política de Privacidade</a></b>. <br>
+            Ao continuar navegando, você declara estar ciente dessas condições.
+        </div>
+        <div class="lgpd--right">
+            <button>ok</button>
+        </div>
+    </div>
+    
+`;
+
+let lsContent = localStorage.getItem('lgpd');
+if(!lsContent){
+    document.body.innerHTML += lgpdHtml;
+
+    let lgpdArea = document.querySelector('.lgpd');
+    let lgpdButton = lgpdArea.querySelector('button')
+
+    lgpdButton.addEventListener('click', async ()=>{
+        lgpdArea.remove();
+
+        let result = await fetch(lgpdUrl);
+        let json = await result.jason();
+
+        if(json.error != ''){
+            localStorage.setItem('lgpd', json.id)
+        }
+
+        
+    });
+}
